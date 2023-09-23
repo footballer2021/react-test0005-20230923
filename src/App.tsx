38d7ce47cd55872d61a30b9d27ffb58439712512
footballer2021 <soccerzuki2013@gmail.com>
2023-09-24 06:13:41 +0900

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react';
 
-function App() {
+const App:React.FC = () => {
+  const ref = useRef<HTMLInputElement>(null);
+  
+  const colorChange = () => {
+    ref.current!.style.color = "red";
+    ref.current!.focus();
+  };
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [playing, setPlaying] = useState(false);
+
+  const playToggle = () => {
+    if(playing){
+      videoRef.current!.pause();
+      setPlaying(false)
+    } else {
+      videoRef.current!.play();
+      setPlaying(true)
+    };
+  };
+
+  let dateNum = () => new Date().getTime();
+  const [numState, setNumState] = useState(dateNum());
+  const numRef = useRef(dateNum());
+
+  console.log(`state:${numState}`);
+  console.log(`ref:${numRef.current}`);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World!</h1>
+      <input type="text" ref={ref} />
+      <div>
+        <button onClick={colorChange}>インプット要素をフォーカスする</button>
+      </div>
+      <hr />
+      <video ref={videoRef}>
+        <source src="ocean.mp4"/>
+      </video>
+      <div>
+        <button onClick={playToggle}>
+          {playing ? "Stop" : "Play"}
+        </button>
+      </div>
+      <hr />
+      <p>state:{numState}</p>
+      <button onClick={() => setNumState(dateNum())}>state更新</button>
+      <p>ref:{numRef.current}</p>
+      <button onClick={() => {numRef.current = dateNum(); console.log(dateNum());}}>ref更新</button>
+    </>
   );
 }
 
